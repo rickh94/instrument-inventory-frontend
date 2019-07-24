80c6335d72d2a87bcc131b3f32a9013f43cfd39f
Rick Henry <fredericmhenry@gmail.com>
2019-07-24 17:01:53 -0400

    remove annoying error handling bug
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- App/App.jsx
+++ App/App.jsx
@@ -48,7 +48,7 @@ class App extends Component {
       this.userHasAuthenticated(true)
     } catch (e) {
       if (e !== 'No current user') {
-        alert(e)
+        console.error(e)
       }
     }
     this.setState({ isAuthenticating: false })
