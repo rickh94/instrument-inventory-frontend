acdcde63399fa7630a17aed2dbecdb03e554566d
Rick Henry <fredericmhenry@gmail.com>
2019-07-26 10:39:10 -0400

    small changes
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- pages/Filter/Filter.jsx
+++ pages/Filter/Filter.jsx
@@ -1,125 +1,67 @@
-import React, { Component, useState } from 'react'
-import PropTypes from 'prop-types'
-import { RootPaper, LoadingHeader, SearchResultsList } from '../../components'
-import {
-  FormControl,
-  FormControlLabel,
-  Checkbox,
-  FormGroup,
-  Button,
-  makeStyles,
-} from '@material-ui/core'
-import { Fields } from '../../components'
-import { lastButton } from '../../globalStyles'
 import { API } from 'aws-amplify'
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import {
+  LoadingHeader,
+  RootPaper,
+  SchemaForm,
+  SearchResultsList,
+} from '../../components'
 
-const useStyles = makeStyles({ lastButton })
-
-const Filter = ({ history, showAlert, filterResults, setFilterResults }) => {
-  const classes = useStyles()
+const Filter = ({ history, schema, showAlert, filterResults, setFilterResults }) => {
   const [isLoading, setLoading] = useState(false)
-  const [instrumentType, setInstrumentType] = useState('')
-  const [size, setSize] = useState('')
-  const [location, setLocation] = useState('')
-  const [notAssigned, setNotAssigned] = useState(false)
-  const [results, setResults] = useState([])
-
-  const setFromEvent = setCallback => event => setCallback(event.target.value)
-
-  const validateForm = () => instrumentType || size || location
-
-  const updateResults = results => {
-    setResults(results)
-    setFilterResults(results)
-  }
-
-  const onSubmit = async event => {
-    event.preventDefault()
-
-    if (!validateForm()) {
-      showAlert('Please select something')
-      return
-    }
-
-    setLoading(true)
+  const [error, setError] = useState(null)
+  const handleSubmit = async body => {
     try {
-      const response = await API.post('instrument-inventory', 'filter', {
-        body: {
-          instrumentType,
-          size,
-          location,
-          notAssigned,
-        },
-      })
+      setLoading(true)
+      const response = await API.post('instrument-inventory', 'filter', { body })
       if (response.length == 0) {
         showAlert('No Matching Instruments Found')
+        setLoading(false)
       } else if (response.length == 1) {
         showAlert('One match found')
         history.push(`/instrument/${response[0].id}`)
       } else {
-        updateResults(response)
+        setFilterResults(response)
+        setLoading(false)
       }
     } catch (err) {
+      setLoading(false)
       if (err.response.data) {
-        showAlert(err.response.data)
+        setError(new Error(err.response.data))
+      } else {
+        showAlert(err)
       }
     }
-
-    setLoading(false)
-  }
-
-  const clearAll = () => {
-    setInstrumentType('')
-    setSize('')
-    setLocation('')
-    setNotAssigned(false)
-    updateResults([])
   }
 
   return (
     <React.Fragment>
       <RootPaper>
         <LoadingHeader isLoading={isLoading} title="Filter Instruments" />
-        <form onSubmit={onSubmit}>
-          <Fields.InstrumentTypeSelect
-            error={false}
-            value={instrumentType}
-            onChange={setFromEvent(setInstrumentType)}
+        {schema && (
+          <SchemaForm
+            schema={schema.components.schemas.InstrumentFilter}
+            onSubmit={handleSubmit}
+            error={error}
           />
-          <Fields.InstrumentSizeSelect
-            error={false}
-            value={size}
-            onChange={setFromEvent(setSize)}
-            instrumentType={instrumentType}
-          />
-          <Fields.LocationSelect
-            error={false}
-            value={location}
-            onChange={setFromEvent(setLocation)}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={notAssigned} onChange={setFromEvent(setNotAssigned)} />
-            }
-            label="Show only unassigned instruments"
-          />
-          <FormGroup row>
-            <Button onClick={clearAll} className={classes.lastButton}>
-              Clear
-            </Button>
-            <Button type="submit" color="primary" disabled={!validateForm()}>
-              Submit
-            </Button>
-          </FormGroup>
-        </form>
+        )}
       </RootPaper>
-      {results.length > 0 && (
+      {filterResults.length > 0 && (
         <RootPaper>
-          <SearchResultsList results={results} />
+          <SearchResultsList results={filterResults} />
         </RootPaper>
       )}
     </React.Fragment>
   )
+}
+
+Filter.propTypes = {
+  schema: PropTypes.object,
+  history: PropTypes.object.isRequired,
+  showAlert: PropTypes.func.isRequired,
+  filterResults: PropTypes.array,
+  setFilterResults: PropTypes.func.isRequired,
 }
 
 export default Filter
