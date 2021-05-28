c9debafbbb4394a4ef2117ca3d044c4c41221579
Rick Henry <rickhenry@rickhenry.dev>
2021-05-28 18:33:52 -0400

    partially implement other inventory items
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/components/inventoryComponents/other/VCreateItem.vue
+++ src/components/inventoryComponents/other/VCreateItem.vue
@@ -1,10 +1,69 @@
 <template>
+  <form @submit="handleSubmit">
+    <div class="flex">
+      <h4 class="text-xl text-gray-900 font-bold mb-2">Create New Item Type</h4>
+    </div>
+    <v-form-control label="Name" label-for="name">
+      <input id="name"
+             name="name"
+             v-model="data.name"
+             type="text"
+             class="appearance-none bg-transparent border-none text-gray-900 py-1 leading-tight"
+      >
+    </v-form-control>
+    <v-form-control label="Count" label-for="count">
+      <input id="count"
+             name="name"
+             v-model="data.count"
+             type="number"
+             class="appearance-none bg-transparent border-none text-gray-900 py-1 leading-tight w-16"
+      >
+    </v-form-control>
+    <v-form-control label="Notes" label-for="notes">
+      <textarea id="notes"
+                v-model="data.notes"
+                class="appearance-none bg-transparent border-none text-gray-900 leading-tight w-full"
+      ></textarea>
+    </v-form-control>
+    <v-spinner v-if="loading" line-fg-color="#805ad5"></v-spinner>
+    <div v-else class="flex justify-end">
+      <button @click.prevent="handleCancel"
+              class="bg-yellow-600 px-4 mx-2 py-2 text-white shadow rounded hover:bg-yellow-800 hover:shadow-lg">Cancel
+      </button>
+      <button type="submit"
+              class="bg-purple-600 px-4 py-2 text-white shadow rounded hover:bg-purple-800 hover:shadow-lg">Submit
+      </button>
+    </div>
+  </form>
 
 </template>
 
 <script>
+import VFormControl from "@/components/UI/VFormControl";
+
 export default {
-  name: "VCreateItem"
+  name: "VCreateItem",
+  components: { VFormControl },
+  data() {
+    return {
+      data: {
+        name: "",
+        count: 0,
+        num_out: 0,
+        signed_out_to: [],
+        notes: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+
+    },
+    handleCancel() {
+      this.data = {name: "", count: 0, num_out: 0, signed_out_to: [], notes: ""}
+      this.$emit('close')
+    }
+  }
 };
 </script>
 
--- src/components/inventoryComponents/other/VInventoryOther.vue
+++ src/components/inventoryComponents/other/VInventoryOther.vue
@@ -2,15 +2,31 @@
   <div>
     <v-spinner v-if="loading" line-fg-color="$805ad5" class="my-2"></v-spinner>
     <div class="flex items-start justify-around flex-wrap mt-4" v-else>
+      <table class="table table-auto mx-4">
+        <thead>
+        <tr class="border-b border-black text-xl">
+          <th class="pr-4">Name</th>
+          <th class="pr-4">Total Count</th>
+          <th class="pr-4">Signed out</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td class="cursor-pointer hover:font-bold" @click="displayItem = item">{{ item.name }}</td>
+          <td>{{ item.count }}</td>
+          <td>{{ item.num_out }}</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
     <div class="flex justify-around max-w-lg mx-auto mt-2">
       <div class="flex justify-around max-w-lg mx-auto mt-2">
-        <button class="bg-purple-600 py-2 px-4 shadow hover:bg-purple-800 hover:shadow-lg rounded text-white"
+        <button class="bg-purple-600 py-2 px-4 shadow hover:bg-purple-800 hover:shadow-lg rounded text-white mr-2"
                 @click.prevent="formComponent = 'v-create-item'" v-if="isAdmin">
           Create Item
         </button>
         <button @click.prevent="formComponent = 'v-use-items'"
-                class="bg-purple-600 py-2 px-4 shadow hover:bg-purple-800 hover:shadow-lg rounded text-white">
+                class="bg-purple-600 py-2 px-4 shadow hover:bg-purple-800 hover:shadow-lg rounded text-white mr-2">
           Use Items
         </button>
         <button @click.prevent="formComponent = 'v-add-items'"
@@ -19,16 +35,79 @@
         </button>
       </div>
     </div>
+    <v-modal v-if="formComponent" @close="formComponent = null" width-class="max-w-lg">
+      <div :is="formComponent" @updated="handleUpdate" :items="items" @close="formComponent = null"></div>
+    </v-modal>
+    <v-modal v-if="displayItem" @close="displayItem = null" width-class="max-w-lg">
+      <div>
+        <span class="font-bold text-gray-700 mb-2">Name: </span>{{ displayItem.name }}
+      </div>
+      <div>
+        <span class="font-bold text-gray-700 mb-2">Count: </span>{{ displayItem.count }}
+      </div>
+      <div>
+        <span class="font-bold text-gray-700 mb-2">Notes: </span>{{ displayItem.notes }}
+      </div>
+      <div>
+        <span class="font-bold text-gray-700 mb-2">Signed Out Count: </span>{{ displayItem.num_out || 0 }}
+      </div>
+      <div>
+        <span class="font-bold text-gray-700 mb-2">Signed Out To: </span>{{ displayItem.signed_out_to | joinOrNull }}
+      </div>
+    </v-modal>
   </div>
 </template>
 
 <script>
 import checkAdmin from "@/mixins/checkAdmin";
+import { API } from "aws-amplify";
+import VModal from "@/components/UI/VModal";
+import VCreateItem from "@/components/inventoryComponents/other/VCreateItem";
 
 export default {
-name: "VInventoryOther",
-  mixins: [checkAdmin]
-}
+  name: "VInventoryOther",
+  components: { VModal, VCreateItem },
+  mixins: [checkAdmin],
+  data() {
+    return {
+      loading: false,
+      items: [],
+      formComponent: null,
+      displayItem: null,
+    };
+  },
+  async created() {
+    try {
+      this.loading = true;
+      const response = await API.get("instrument-inventory", "other", {});
+      this.items = response.items;
+      this.loading = false;
+    } catch (e) {
+      this.loading = false;
+      if (e.response) {
+        this.$toasted.error(e.response.data, { duration: 2000 });
+      } else {
+        this.$toasted.error(e.toString(), { duration: 2000 });
+        console.error(e);
+      }
+    }
+  },
+  methods: {
+    handleUpdate({updatedIds, updatedItems}) {
+      this.loading = true
+      this.itmes = [...this.items.filter(({id}) => !updatedIds.includes(id)), ...updatedItems]
+      this.loading = false
+    }
+  },
+  filters: {
+    joinOrNull(value) {
+      if (value && value.length) {
+        return value.join(', ')
+      }
+      return "";
+    }
+  }
+};
 </script>
 
 <style scoped>
