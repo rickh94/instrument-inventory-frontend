dc17765a10516583764e2c25e9bb2739f10a2d15
Rick Henry <rickhenry@rickhenry.dev>
2021-08-31 17:21:48 -0400

    hard code config for testing
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-undef */
-const dev = {
-  stage: 'dev',
-  apiGateway: {
-    REGION: 'us-east-1',
-    URL: 'https://r8vz95noef.execute-api.us-east-1.amazonaws.com/dev/',
-  },
-  cognito: {
-    REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_E8bTuMdrD',
-    APP_CLIENT_ID: '4uk5fpfv8tji91s1e1j86hvb35',
-    IDENTITY_POOL_ID: 'us-east-1:c6ffd490-2cbf-4465-9c62-657ec10924b1',
-    DOMAIN: 'localhost',
-    // DOMAIN:
-    //   process.env.USER === 'rick'
-    //     ? 'localhost'
-    //     : process.env.DEPLOY_URL.replace('https://', ''),
-    SECURE: process.env.USER === 'rick' ? false : true,
-  },
-  s3: {
-    REGION: 'us-east-1',
-    BUCKET: 'instrument-inventory-dev-photosbucket-10qe9d73yvjmj',
-  },
-}
+// const dev = {
+//   stage: 'dev',
+//   apiGateway: {
+//     REGION: 'us-east-1',
+//     URL: 'https://r8vz95noef.execute-api.us-east-1.amazonaws.com/dev/',
+//   },
+//   cognito: {
+//     REGION: 'us-east-1',
+//     USER_POOL_ID: 'us-east-1_E8bTuMdrD',
+//     APP_CLIENT_ID: '4uk5fpfv8tji91s1e1j86hvb35',
+//     IDENTITY_POOL_ID: 'us-east-1:c6ffd490-2cbf-4465-9c62-657ec10924b1',
+//     DOMAIN: 'localhost',
+//     // DOMAIN:
+//     //   process.env.USER === 'rick'
+//     //     ? 'localhost'
+//     //     : process.env.DEPLOY_URL.replace('https://', ''),
+//     SECURE: process.env.USER === 'rick' ? false : true,
+//   },
+//   s3: {
+//     REGION: 'us-east-1',
+//     BUCKET: 'instrument-inventory-dev-photosbucket-10qe9d73yvjmj',
+//   },
+// }
 
 const prod = {
   stage: 'prod',
