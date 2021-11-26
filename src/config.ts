ffee3f8d123391cfa8527be05e17c93367492716
Rick Henry <rickhenry@rickhenry.dev>
2021-11-26 18:48:43 -0500

    add demo mode
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/config.ts
+++ src/config.ts
@@ -1,48 +1,62 @@
 /* eslint-disable no-undef */
 const dev = {
-  stage: 'dev',
+  stage: "dev",
   apiGateway: {
-    REGION: 'us-east-1',
-    URL: 'https://r8vz95noef.execute-api.us-east-1.amazonaws.com/dev/',
+    REGION: "us-east-1",
+    URL: "https://r8vz95noef.execute-api.us-east-1.amazonaws.com/dev/"
   },
   cognito: {
-    REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_E8bTuMdrD',
-    APP_CLIENT_ID: '4uk5fpfv8tji91s1e1j86hvb35',
-    IDENTITY_POOL_ID: 'us-east-1:c6ffd490-2cbf-4465-9c62-657ec10924b1',
-    DOMAIN: 'localhost',
-    // DOMAIN:
-    //   process.env.USER === 'rick'
-    //     ? 'localhost'
-    //     : process.env.DEPLOY_URL.replace('https://', ''),
-    SECURE: false,
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_E8bTuMdrD",
+    APP_CLIENT_ID: "4uk5fpfv8tji91s1e1j86hvb35",
+    IDENTITY_POOL_ID: "us-east-1:c6ffd490-2cbf-4465-9c62-657ec10924b1",
+    DOMAIN: "localhost",
+    SECURE: false
+  }
+};
+
+const demo = {
+  stage: "demo",
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://0sayiu8gd1.execute-api.us-east-1.amazonaws.com/demo/"
   },
-  // s3: {
-  //   REGION: 'us-east-1',
-  //   BUCKET: 'instrument-inventory-dev-photosbucket-10qe9d73yvjmj',
-  // },
-}
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_mdlcscRdA",
+    APP_CLIENT_ID: "220hvavn2s21c6gcgfg7mvt080",
+    IDENTITY_POOL_ID: "us-east-1:2bc7b531-0d39-4489-ad87-0f7a4ea33eff",
+    DOMAIN: "instrument-demo.rickhenry.dev",
+    SECURE: true
+  }
+};
+
 
 const prod = {
-  stage: 'prod',
+  stage: "prod",
   apiGateway: {
-    REGION: 'us-east-1',
-    URL: 'https://3dw921ejuh.execute-api.us-east-1.amazonaws.com/prod/',
+    REGION: "us-east-1",
+    URL: "https://3dw921ejuh.execute-api.us-east-1.amazonaws.com/prod/"
   },
   cognito: {
-    REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_x5p2txsmA',
-    APP_CLIENT_ID: '7rvmg6d2cklqe6jrt3mscbqba7',
-    IDENTITY_POOL_ID: 'us-east-1:95a1e528-85e0-4e89-ac57-810a8f2219ae',
-    DOMAIN: 'tmminstruments.com',
-    SECURE: true,
-  },
-  // s3: {
-  //   REGION: 'us-east-1',
-  //   BUCKET: 'instrument-inventory-prod-photosbucket-1gn4run79aalw',
-  // },
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_x5p2txsmA",
+    APP_CLIENT_ID: "7rvmg6d2cklqe6jrt3mscbqba7",
+    IDENTITY_POOL_ID: "us-east-1:95a1e528-85e0-4e89-ac57-810a8f2219ae",
+    DOMAIN: "tmminstruments.com",
+    SECURE: true
+  }
+};
+
+let config;
+
+if (process.env.VUE_APP_DEMO === "TRUE") {
+  config = demo;
+} else if (process.env.NODE_ENV === "production") {
+  config = prod;
+} else {
+  config = dev;
 }
 
-const config = process.env.NODE_ENV === 'production' ? prod : dev
 
-export default config
+export default config;
